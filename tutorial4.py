9a368db586b71e88178c235bc7dd1c2558b2db88
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-10-17 10:51:30 -0400

    Updated from Brython Server: 10/17/2018 10:51:30 AM

--- tutorial4.py
+++ tutorial4.py
@@ -1,4 +1,5 @@
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
 class SpaceGame(App):
     """
     Tutorial4 space game example.
@@ -8,10 +9,9 @@ class SpaceGame(App):
         # Background
         black = Color(0, 1)
         noline = LineStyle(0, black)
-        bg_asset = RectangleAsset(myapp.width, myapp.height, noline, black)
+        bg_asset = RectangleAsset(self.width, self.height, noline, black)
         bg = Sprite(bg_asset, (0,0))
 
-myapp = SpaceGame()
-
+myapp = SpaceGame() # what is wrong with this picture
 
 myapp.run()
